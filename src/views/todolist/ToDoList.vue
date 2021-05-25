c7370dae0771f79eaa4b48cd2c211baaaaedb374
age68573 <s1410523045@gms.nutc.edu.tw>
2021-05-25 09:58:43 +0800

    todolist 拆分組件

--- src/views/todolist/ToDoList.vue
+++ src/views/todolist/ToDoList.vue
@@ -36,25 +36,7 @@
         
       </v-list-item>
       
-    <v-list-item v-for="(item,index) in list" :key="index" @click="itemCheck(index)">
-      <template>
-        <v-list-item-action>
-          <v-checkbox
-            :input-value="item.checked"
-            color="primary"
-          ></v-checkbox>
-        </v-list-item-action>
-
-        <v-list-item-content>
-          <v-list-item-title>{{item.title}}</v-list-item-title>
-          <v-list-item-subtitle>{{date}}</v-list-item-subtitle>
-        </v-list-item-content>
-        <v-btn @click.stop="deleteTodo(index)"
-          elevation="2"
-          color="error"
-        >DELETE</v-btn>
-      </template>
-    </v-list-item>
+    <to-do-list-item :list="list" @itemCheck="itemCheck" @deleteTodo="deleteTodo" />
     </v-list-item-group>
     </v-list>
     <v-container>
@@ -78,7 +60,11 @@
 </template>
 
 <script>
+import ToDoListItem from './childComps/ToDoListItem'
 export default {
+  components: {
+    ToDoListItem
+  },
   data() {
     return {
       todo: null,
---
+++ src/views/todolist/childComps/ToDoListItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-list-item v-for="(item,index) in list" :key="index" @click="itemCheck(index)">
+    <template>
+      <v-list-item-action>
+        <v-checkbox
+          :input-value="item.checked"
+          color="primary"
+        ></v-checkbox>
+      </v-list-item-action>
+
+      <v-list-item-content>
+        <v-list-item-title>{{item.title}}</v-list-item-title>
+        <v-list-item-subtitle>{{date}}</v-list-item-subtitle>
+      </v-list-item-content>
+      <v-btn @click.stop="deleteTodo(index)"
+        elevation="2"
+        color="error"
+      >DELETE</v-btn>
+    </template>
+  </v-list-item>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      date: this.$getNowFormatDate(),
+    }
+  },
+  methods: {
+    itemCheck(index) {
+      this.$emit('itemCheck', index)
+    },
+    deleteTodo(index) {
+      this.$emit('deleteTodo', index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
