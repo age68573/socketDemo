6129dbb80833aa609b5d94594f644ab68ecda0b4
age68573 <s1410523045@gms.nutc.edu.tw>
2021-05-27 09:47:39 +0800

    fix login page css

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,15 +1,58 @@
 <template>
-  <div>
-    <h2>login</h2>
+  <div class="login">
+    <v-container class="box">
+      <div class="login-item">
+        <v-row justify='center'>
+        <h1>DEMO</h1>
+      </v-row>
+      <v-row justify='center'>
+        <v-col cols="4" class="pb-0">
+          <v-text-field
+            label="Account"
+            :rules="rules"
+            hide-details="auto"
+          ></v-text-field>
+          <v-text-field label="Password"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row justify='center'>
+        <v-col cols="4">
+          <v-btn
+            block
+            color="primary"
+          >
+            Login
+          </v-btn>
+        </v-col>
+        
+        
+      </v-row>
+      </div>
+      
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+    ]
+  })
 }
 </script>
 
 <style scoped>
+.container{
+  position: relative;
+}
+.box {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
 
 </style>
